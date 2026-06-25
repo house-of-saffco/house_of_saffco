7a0f885395b0bda82372016134dfaadad9fff3c6
house-of-saffco <283557493+house-of-saffco@users.noreply.github.com>
2026-06-25 10:28:37 +0400

    added the canonical url

--- src/data/seo.js
+++ src/data/seo.js
@@ -1,4 +1,4 @@
-export const SITE_URL = 'https://houseofsaffco.com'
+export const SITE_URL = 'https://www.houseofsaffco.com'
 
 /** @type {Record<string, { title: string; description: string }>} */
 export const PAGE_SEO = {
